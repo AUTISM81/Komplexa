c9ba788ca0679a8f72ad727e765c60eb6f2b36fc
AUTISM81 <christian.backstrom@elev.ga.ntig.se>
2020-02-07 11:18:50 +0100

    Done with args, addition and multiply

--- src/Komplex.java
+++ src/Komplex.java
@@ -2,24 +2,50 @@ public class Komplex {
     private  double real;
     private double img;
 
-    public Komplex(double img, double real) {
+    public Komplex(double real, double img) {
         this.real = real;
         this.img = img;
-    }
-
-    public double getImg() {
-        return img;
     }
 
     public double getReal() {
         return real;
     }
 
+    public double getImg() {
+        return img;
+    }
+
     public Komplex add(Komplex b) {
         double real = this.getReal() + b.getReal();
         double img = this.getImg() + b.getImg();
 
-        return new Komplex(img, real);
+        return new Komplex(real, img);
+    }
+
+    public Komplex multiply(Komplex b) {
+        double real = (this.getReal()*b.getReal()) - (this.getImg()*b.getImg());
+        double img = (this.getImg()*b.getReal()) + (this.getReal()*b.getImg());
+
+        return new Komplex(real, img);
+    }
+
+    public double arg() {
+        double arctan = 0;
+        if (this.getReal() > 0){
+            arctan = this.getImg()/this.getReal();
+        } else if (this.getImg() >= 0 || this.getReal() < 0){
+            arctan = (this.getImg()/this.getReal()) + Math.PI;
+        } else if (this.getImg() < 0 || this.getReal() < 0){
+            arctan = (this.getImg()/this.getReal()) - Math.PI;
+        } else if (this.getImg() > 0 || this.getReal() == 0){
+            arctan = Math.PI/2;
+        } else if (this.getImg() < 0 || this.getReal() == 0){
+            arctan = (Math.PI/2)*-1;
+        } else {
+            System.out.print("Undefined");
+        }
+
+        return arctan;
     }
 
     @Override
--- src/komplexTest.java
+++ src/komplexTest.java
@@ -1,9 +1,17 @@
 public class komplexTest {
     public static void main(String[] args) {
-        Komplex a = new Komplex(3, 2);
-        Komplex b = new Komplex(4, 1);
+        Komplex a = new Komplex(3,2);
+        Komplex b = new Komplex(4,1);
 
-        a.add(b);
-        System.out.print(a);
+
+        //multiplikation
+        Komplex c = a.multiply(b);
+        System.out.print(c);
+
+
+        //addition
+        Komplex z = a.add(b);
+        System.out.print(z);
+
     }
 }
